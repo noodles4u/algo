2cc0e6b1d06b81cc45e4a8d899a20aa2b67e6bf3
noodles4u <louisdesmett@gmail.com>
2023-05-23 16:25:57 +0200

    array_printer

---
+++ array_printer.js
@@ -0,0 +1,15 @@
+// PRINT NUMBERS
+// Write an algorithm which verify if a given positive integer is a perfect number, meaning equal to the sum of his divisors (except himself).
+
+// Required line for prompt-sync, enables prompts in terminal with node
+const prompt = require('prompt-sync')({ sigint: true });
+//
+// let userInput = parseFloat(prompt("What's your chosen number?"));
+let userInput = prompt("What's input?");
+let blob = 'I like to samba samba';
+let arrayLibrary = ['test', 'sampling', blob];
+const arrayInput = () => {
+  arrayLibrary.push(userInput);
+  console.log(arrayLibrary);
+};
+arrayInput();
